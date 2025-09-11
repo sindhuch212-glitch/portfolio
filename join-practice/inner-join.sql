38a4422078c7fe34f46191f2b8e55a77ed14951f
Sindhu <sindhuch212@gmail.com>
2025-09-11 12:15:12 +0530

    Add INNER JOIN example

--- join-practice/inner-join.sql
+++ join-practice/inner-join.sql
@@ -1,4 +1,6 @@
 -- INNER JOIN Example
+-- This query lists all orders along with the customer names.
+-- Only customers who have placed at least one order will be included in the results.
 SELECT o.OrderID, c.Name, o.Product
 FROM Orders o
 INNER JOIN CustomerData c
